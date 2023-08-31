2a066c85b261c4c100a6175bc01092645aa1d6a5
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-08-31 19:39:41 +0200

    Added more types and XML docs

--- src/Devices/Common.cs
+++ src/Devices/Common.cs
@@ -1,10 +1,14 @@
-﻿namespace Nefarius.Utilities.HID.Devices;
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Nefarius.Utilities.HID.Devices;
 
 /// <summary>
-///     Possible D-Pad directions.
+///     Possible D-Pad directions for DualShock™/DualSense™ compatible reports.
 /// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
 public enum DPadDirection
 {
+#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
     Default = 0x8,
     NorthWest = 0x7,
     West = 0x6,
@@ -14,4 +18,34 @@ public enum DPadDirection
     East = 0x2,
     NorthEast = 0x1,
     North = 0x0
+#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
+}
+
+/// <summary>
+///     Possible axis ranges.
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public enum AxisRangeType
+{
+    /// <summary>
+    ///     Axis range is represented by a signed 16-bit integer. The value is between -32768 and 32767.
+    /// </summary>
+    Short,
+
+    /// <summary>
+    ///     Axis range is represented by an unsigned byte. The value is between 0 and 255.
+    /// </summary>
+    Byte
+}
+
+/// <summary>
+///     Possible trigger ranges.
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
+public enum TriggerRangeType
+{
+    /// <summary>
+    ///     Axis range is represented by an unsigned byte. The value is between 0 and 255.
+    /// </summary>
+    Byte
 }
