8a79757ab47665eff3d94b921c3a3e09262b4f80
Benjamin Höglinger-Stelzer <nefarius@dhmx.at>
2023-08-31 17:50:42 +0200

    Update BitHelpers.cs

--- src/Util/BitHelpers.cs
+++ src/Util/BitHelpers.cs
@@ -1,5 +1,11 @@
-﻿namespace Nefarius.Utilities.HID.Util;
+﻿using System.Diagnostics.CodeAnalysis;
 
+namespace Nefarius.Utilities.HID.Util;
+
+/// <summary>
+///     Bit-wise manipulation utilities.
+/// </summary>
+[SuppressMessage("ReSharper", "UnusedMember.Global")]
 public static class BitHelpers
 {
     private static readonly uint[] BitCountConversions =
@@ -9,31 +15,73 @@ public static class BitHelpers
         0x7FFFFFF, 0xFFFFFFF, 0x1FFFFFFF, 0x3FFFFFFF, 0x7FFFFFFF, 0xFFFFFFFF
     };
 
+    /// <summary>
+    ///     Extracts one or more bits from a given value.
+    /// </summary>
+    /// <param name="value">The value to read from.</param>
+    /// <param name="offset">The offset of the bit of interest.</param>
+    /// <param name="count">The amount of bits to include.</param>
+    /// <returns>The extracted value made from the given mask.</returns>
     public static byte GetBitsAsByte(this byte value, byte offset, byte count)
     {
         return (byte)((value >> offset) & BitCountConversions[count]);
     }
 
+    /// <summary>
+    ///     Extracts one or more bits from a given value.
+    /// </summary>
+    /// <param name="value">The value to read from.</param>
+    /// <param name="offset">The offset of the bit of interest.</param>
+    /// <param name="count">The amount of bits to include.</param>
+    /// <returns>The extracted value made from the given mask.</returns>
     public static byte GetBitsAsByte(this ushort value, byte offset, byte count)
     {
         return (byte)((value >> offset) & BitCountConversions[count]);
     }
 
+    /// <summary>
+    ///     Extracts one or more bits from a given value.
+    /// </summary>
+    /// <param name="value">The value to read from.</param>
+    /// <param name="offset">The offset of the bit of interest.</param>
+    /// <param name="count">The amount of bits to include.</param>
+    /// <returns>The extracted value made from the given mask.</returns>
     public static short GetBitsAsShort(this ushort value, byte offset, byte count)
     {
         return (short)((value >> offset) & BitCountConversions[count]);
     }
 
+    /// <summary>
+    ///     Extracts one or more bits from a given value.
+    /// </summary>
+    /// <param name="value">The value to read from.</param>
+    /// <param name="offset">The offset of the bit of interest.</param>
+    /// <param name="count">The amount of bits to include.</param>
+    /// <returns>The extracted value made from the given mask.</returns>
     public static byte GetBitsAsByte(this uint value, byte offset, byte count)
     {
         return (byte)((value >> offset) & BitCountConversions[count]);
     }
 
+    /// <summary>
+    ///     Extracts one or more bits from a given value.
+    /// </summary>
+    /// <param name="value">The value to read from.</param>
+    /// <param name="offset">The offset of the bit of interest.</param>
+    /// <param name="count">The amount of bits to include.</param>
+    /// <returns>The extracted value made from the given mask.</returns>
     public static short GetBitsAsShort(this uint value, byte offset, byte count)
     {
         return (short)((value >> offset) & BitCountConversions[count]);
     }
 
+    /// <summary>
+    ///     Extracts one or more bits from a given value.
+    /// </summary>
+    /// <param name="value">The value to read from.</param>
+    /// <param name="offset">The offset of the bit of interest.</param>
+    /// <param name="count">The amount of bits to include.</param>
+    /// <returns>The extracted value made from the given mask.</returns>
     public static int GetBitsAsInt(this uint value, byte offset, byte count)
     {
         return (int)((value >> offset) & BitCountConversions[count]);
